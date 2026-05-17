4a9efa7bb290a8d4ea71b706d3696e7649e5c9e5
s83757 <kennethren271@gmail.com>
2026-05-16 18:01:42 -0700

    implemented homepage sidebar to work with sidebar

--- src/utils/api/userApi.js
+++ src/utils/api/userApi.js
@@ -21,3 +21,25 @@ export async function getUserByEmail(email) {
 
   return data;
 }
+
+export async function getAllUsers() {
+
+  const res = await fetch(
+    `${BACKEND_API_URL}/getAllUsers`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query: "all" })
+    }
+  );
+
+  const data = await res.json()
+
+  if (!res.ok) {
+    throw new Error(data.error)
+  }
+
+  return data;
+}

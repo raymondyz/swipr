324bcc2d433b090a2a8aea0345d816e620164941
s83757 <kennethren271@gmail.com>
2026-05-28 19:05:50 -0700

    fix search page bug

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,12 +4,14 @@ function ProfileCard({profile}) {
   const swipe_availability = profile.swipe_availability
   const location_preferences = profile.location_preferences
   const notes = profile.notes
+  
 
   return <div style={{border: "1px solid black"}}>
     <h2>{name}</h2>
     <p>{username}</p>
     <h3>Swipe: {swipe_availability}</h3>
-    {location_preferences && location_preferences.map(location => <p>{location}</p>)}
+    
+    {location_preferences && Object.keys(location_preferences).map(location => <p>{location}</p>)}
   </div>
 }
 

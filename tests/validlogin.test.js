5a3417a5234381a67bcc8490e0c8a3145ee6b7fd
Raymond Zou <raymond.yh.zou@gmail.com>
2026-06-01 02:07:35 -0700

    Fix playwright tests

--- tests/validlogin.test.js
+++ tests/validlogin.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('can go to sign in link', async ({ page }) => {
     await page.goto('/');
-    await page.getByRole('button', {name: 'Signup Page'}).click();
+    await page.getByText('Create Account').click();
     await page.locator('#name').fill('big boy');
     await page.locator('#username').fill('big boy2');
     await page.locator('#email').fill('megachad@g.ucla.edu');
